6b43ba2a73c6b63975919fb2e2069bced19d8ff2
nonoesimposible <nonoesimposible72@gmail.com>
2022-02-15 10:57:25 -0700

    add a Dwarf test

--- MiddleEarth.java
+++ MiddleEarth.java
@@ -6,6 +6,7 @@ public class MiddleEarth {
     public static void main(String[] args) {
         ArrayList<Combatant> characters = new ArrayList<Combatant>();
         characters.add(new Elf("Legolas"));
+        characters.add(new Dwarf("Bofur"));
         characters.add(new Man("Strider"));
         characters.add(new GondorMan("Faramir"));
         characters.add(new RohanMan("Éomer"));
