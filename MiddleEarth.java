56166f52a2875c68f5e37423088a0af7ed89fbc6
nonoesimposible <nonoesimposible72@gmail.com>
2022-02-15 09:37:42 -0700

    cool battle, but not a proper noun (yet)

--- MiddleEarth.java
+++ MiddleEarth.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class MiddleEarth {
 
     public static void main(String[] args) {
-        // This is where we will put our code to run our Battle for Middle Earth!
+        // This is where we will put our code to run our battle for Middle Earth!
     }
 
 }
