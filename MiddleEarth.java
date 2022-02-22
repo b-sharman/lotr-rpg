cb75d29afab38c19f8b2cf3583bc011395d7d856
nonoesimposible <nonoesimposible72@gmail.com>
2022-02-22 10:46:53 -0700

    add a printHealths method that displays all healths

--- MiddleEarth.java
+++ MiddleEarth.java
@@ -3,6 +3,9 @@ import java.util.Random;
 
 public class MiddleEarth {
 
+    // number of spaces in a tab - change to match environment
+    public static final int TAB_LENGTH = 8;
+
     public static void main(String[] args) {
         ArrayList<Combatant> characters = new ArrayList<Combatant>();
         characters.add(new Elf("Legolas"));
@@ -35,6 +38,37 @@ public class MiddleEarth {
             defender = getRandomCombatant(combatants);
         }
         return defender;
+    }
+
+    public static void printHealths(ArrayList<Combatant> combatants) {
+        System.out.println();
+        System.out.println("***HEALTH BOARD***");
+
+        // find the length of the longest name
+        int max_length = 0;
+        for (Combatant c : combatants) {
+            if (c.getName().length() > max_length) {
+                max_length = c.getName().length();
+            }
+        }
+
+        // for each Combatant in the List...
+        for (Combatant c : combatants) {
+            System.out.print(c.getName());
+
+            // print enough tabs that all the health bars are aligned
+            for (int i=0; i<((c.getName().length() - max_length) / TAB_LENGTH) + 1; i++) {
+                System.out.print("\t");
+            }
+
+            // 2 half-block characters for every health point
+            for (int i=0; i<c.getHealth() * 2; i++) {
+                // System.out.print("█");
+                System.out.print("▀");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
 }
